e41b6f42cb98930e3a4f46826bc40f676d62bb56
LuisFerFlores31 <a01738347@tec.mx>
2024-10-02 08:42:22 -0600

    prueba poo

--- Act 2.1/Act 2.1.cpp
+++ Act 2.1/Act 2.1.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-    int opc = 0;
-    cin>>opc;
+class Listaligada{
 
-    switch(opc){
-        
-        case 0:
-        cout<<"case 0 BORRAR"<<endl;
-        break;
+    public:
+    Listaligada();
 
-        case 1:
-        cout<<"case 1 BORRAR"<<endl;
-        break;
-
-        case 2:
-        cout<<"case 2 BORRAR"<<endl;
-        break;
-
-        case 3:
-        cout<<"case 3 BORRAR"<<endl;
-        break;
-
-        case 4:
-        cout<<"case 4 BORRAR"<<endl;
-        break;
-
-        case 5:
-        cout<<"case 5 BORRAR"<<endl;
-        break;
+    void insertarInicio(int valor){
+        cout<<"insertar inicio"<<endl;
+    }
+    void insertarFinal(int valor){
+        cout<<"insertar al final"<<endl;
     }
 
+    private:
+    int valor;
+};
 
+int main (){
+    ListaLigada lista;
+    int opc, valor;
+   do {
+        cin >> opc;
+        switch (opc) {
+        case 1: // Insertar al inicio
+            cin >> valor;
+            cout<<valor<<endl;
+            lista.insertarInicio(valor);
+            break;
+        case 2: // Insertar al final
+            cin >> valor;
+            lista.insertarFinal(valor);
+            break;
+        case 3: // Eliminar al inicio
+            //lista.eliminarInicio();
+            break;
+        case 4: // Eliminar al final
+            //lista.eliminarFinal();
+            break;
+        case 5: // Imprimir la lista
+            //lista.imprimir();
+            break;
+        case 0: // Salir
+            break;
+        default:
+            break;
+        }
+    } while (opc != 0);
     return 0;
 };
