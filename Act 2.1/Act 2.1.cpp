150c9e4e8ea0f76cf0eb679f1050be7c77ea5d2c
LuisFerFlores31 <a01738347@tec.mx>
2024-10-02 09:53:09 -0600

    1,2,3,5 listos y arreglados

--- Act 2.1/Act 2.1.cpp
+++ Act 2.1/Act 2.1.cpp
@@ -7,17 +7,15 @@ using namespace std;
     };
 
     void insertarInicio(node* &pth,int valor){
-        cout<<"insertar inicio"<<endl;
-         node *tmp = new node;
+        node *tmp = new node;
         tmp -> data = valor;
         tmp -> next = pth;
         pth = tmp;
     }
     void insertarFinal(node* &pth,int valor){
-        cout<<"insertar al final"<<endl;
-         node* newNode = new node;
-        newNode -> data = valor;
+        node* newNode = new node;
         newNode -> next = nullptr;
+        newNode -> data = valor;
 
         if (pth== nullptr)
         pth = newNode;
@@ -31,8 +29,16 @@ using namespace std;
         }
     }
 
-    void eliminarInicio(){
-        cout<<"eliminar inicio"<<endl;
+    void eliminarInicio(node* &pth){
+        if (pth == nullptr)
+            cout<<"ERROR"<<endl;
+
+        else{
+            cout<<"eliminar inicio"<<endl;
+             node *tmp = pth;
+            pth = pth -> next;
+            delete tmp;
+        }
     }
 
     void eliminarFinal(){
@@ -60,7 +66,6 @@ int main (){
         switch (opc) {
         case 1: // Insertar al inicio
             cin >> valor;
-            cout<<valor<<endl;
             insertarInicio(list,valor);
             break;
         case 2: // Insertar al final
@@ -68,7 +73,7 @@ int main (){
             insertarFinal(list,valor);
             break;
         case 3: // Eliminar al inicio
-            eliminarInicio();
+            eliminarInicio(list);
             break;
         case 4: // Eliminar al final
             eliminarFinal();
