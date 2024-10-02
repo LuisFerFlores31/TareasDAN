39d659b69a404a5dbf6de0a5d1c8567b67ccd9d9
LuisFerFlores31 <a01738347@tec.mx>
2024-10-01 20:07:27 -0600

    menu

--- Act 2.1/Act 2.1.cpp
+++ Act 2.1/Act 2.1.cpp
@@ -2,7 +2,35 @@
 using namespace std;
 
 int main (){
-    cout<<"test"<<endl;
+    int opc = 0;
+    cin>>opc;
+
+    switch(opc){
+        
+        case 0:
+        cout<<"case 0 BORRAR"<<endl;
+        break;
+
+        case 1:
+        cout<<"case 1 BORRAR"<<endl;
+        break;
+
+        case 2:
+        cout<<"case 2 BORRAR"<<endl;
+        break;
+
+        case 3:
+        cout<<"case 3 BORRAR"<<endl;
+        break;
+
+        case 4:
+        cout<<"case 4 BORRAR"<<endl;
+        break;
+
+        case 5:
+        cout<<"case 5 BORRAR"<<endl;
+        break;
+    }
 
 
     return 0;
