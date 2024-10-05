206f393624a33ce649fe566f90fb56fec2d0d58b
LuisFerFlores31 <a01738347@tec.mx>
2024-10-04 18:02:02 -0600

    l

---
+++ Act2.3/Act2.3.cpp
@@ -0,0 +1,15 @@
+/*Act 2.1 - Implementación de un ADT de estructura de datos lineales
+Luis Fernando Cruz Flores A01738347
+Antonio Mendez Rodriguez A01738269
+Luis Angel Zempoalteca Serrano A01738862
+
+Fecha: 9 de Octubre*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    
+    return 0;  
+    
+};
