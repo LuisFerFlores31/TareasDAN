d3dc5d1df9a71525701a75d0585b26a0b8f356bf
LuisFerFlores31 <a01738347@tec.mx>
2024-10-09 09:14:06 -0600

    .

--- Act2.3/Act2.3.cpp
+++ Act2.3/Act2.3.cpp
@@ -4,16 +4,43 @@ Antonio Mendez Rodriguez A01738269
 Luis Angel Zempoalteca Serrano A01738862
 
 Fecha: 9 de Octubre*/
-
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 struct Node {
     int data;
     Node* next;
+    Node* previous;
 };
 
+// Estructura para almacenar los registros
+struct Registro {
+    int mes;
+    int dia;
+    string contenido;
+    string fecha; 
+}; 
+
+
 int main (){
+    //open File 
+    ifstream file("bitacora.txt");
+    if (!file.is_open()) {
+        cout << "El archivo no se pudo abrir" << endl;
+        return 1;
+    }
+
+
+     // resultados en sorted.txt
+    ofstream output("SortedData.txt");
+    if (!output.is_open()) {
+        cout << "No se pudo abrir el archivo de salida" << endl;
+        return 1;
+    }
 
     return 0;  
     
