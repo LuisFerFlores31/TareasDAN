28f9fc056d9cd22b02d012d4e581c58bd64ffb3f
LuisFerFlores31 <a01738347@tec.mx>
2024-10-30 22:08:35 -0600

    Ahora si Quedo el POO con complejidad

--- Act3.2/Act3.2POO.cpp
+++ Act3.2/Act3.2POO.cpp
@@ -1,3 +1,10 @@
+/*
+Equipo:
+Antonio Méndez Rodríguez A01738269
+Luis Fernando Flores Cruz A017388347
+Luis Angel Zempoalteca Serrano A01738862
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -5,18 +12,21 @@ using namespace std;
 class MaxHeap {
 private:
     vector<int> heap;
-
+    //Complejidad O(log n)
     void heapify(int n, int i) {
         int largest = i;         
         int left = 2 * i + 1;
         int right = 2 * i + 2;
 
+        // Si el hijo izquierdo es mayor que la raíz
         if (left < n && heap[left] > heap[largest])
             largest = left;
 
+         // Si el hijo derecho es mayor que la raíz
         if (right < n && heap[right] > heap[largest])
             largest = right;
 
+         // Si el más grande no es la raíz
         if (largest != i) {
             swap(heap[i], heap[largest]);
             heapify(n, largest);
@@ -24,31 +34,28 @@ private:
     }
 
 public:
+    //Complejidad O(log n)
     void push(int val) {
         heap.push_back(val);
         for (int i = heap.size() / 2 - 1; i >= 0; i--) {
             heapify(heap.size(), i);
         }
     }
-
-    void pop() {
-        if (!heap.empty()) {
-            cout << heap[0] << endl;
-            heap[0] = heap.back();
-            heap.pop_back();
-            heapify(heap.size(), 0);
-        } else {
-            cout << "Heap is empty!" << endl;
-        }
-    }
-
+    //Complejidad O(log n)
+    void pop(){
+        cout << heap[0] << endl;
+        heap[0] = heap.back();
+        heap.pop_back();
+        heapify(heap.size(), 0);
+    } 
+    //Complejidad O(n)
     void print() const {
         for (int i : heap) {
             cout << i << " ";
         }
         cout << endl;
     }
-
+    //Complejidad O(1)
     int top() const {
         if (!heap.empty()) {
             return heap[0];
@@ -57,7 +64,7 @@ public:
             return -1; // valor de error si el heap está vacío
         }
     }
-
+    //Complejidad O(1)
     void isempty() {
         if (heap.size() == 0){
             cout<<"true"<<endl;
@@ -66,11 +73,7 @@ public:
             cout<<"flase"<<endl;
         }
 }
-
-    //bool isempty() const {
-    //    return heap.empty();
-    //}
-
+    //Complejidad O(1)
     int size() const {
         return heap.size();
     }
